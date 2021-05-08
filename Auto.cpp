084362eb367ccd47b98f00d536f34e79128e4b79
sforn-null <1641832926@qq.com>
2021-05-08 22:12:16 +0800

    auto关键字

---
+++ Auto.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+//auto关键字
+
+
+//auto不能作为参数，也不能用于数组
+int main()
+{
+	int a = 0;
+	auto b = a;//b的类型是根据a的类型推导出int
+
+	cout << typeid(a).name() << endl;
+	//可以看变量的类型
+	cout << typeid(a).name() << endl;
+
+	return 0;
+}
