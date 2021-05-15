50dd2d9ffdd42a9b34fa19397216a2b74d8bf539
sforn-null <1641832926@qq.com>
2021-05-15 23:20:59 +0800

    string的测试

---
+++ test_string.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+#include"string.h"
+
+int main()
+{
+	//shy::test_string1();
+	//shy::test_string2();
+	//shy::test_string3();
+	//shy::test_string4();
+	shy::test_string3();
+	return 0;
+}
