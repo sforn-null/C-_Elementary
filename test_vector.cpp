693a54a8e8030c8f694a3a814ba69436dae014d9
sforn-null <1641832926@qq.com>
2021-05-16 22:48:57 +0800

    vector的测试

---
+++ test_vector.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+#include"vector.h"
+
+
+int main()
+{
+	//shy::test_vector1();
+	//shy::test_vector2();
+
+	//shy::test_vector3();
+	shy::test_vector4();
+	return 0;
+}
