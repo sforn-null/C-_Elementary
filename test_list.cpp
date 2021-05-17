b269c206b14f2a9d21c59e636a6fcdea2f3dbe34
sforn-null <1641832926@qq.com>
2021-05-17 22:34:30 +0800

    list的测试

---
+++ test_list.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<assert.h>
+using namespace std;
+#include"list.h"
+
+int main()
+{
+	//shy::test_list1();
+	//shy::test_list2();
+	//shy::test_list3();
+	//shy::test_list4();
+	shy::test_list5();
+}
